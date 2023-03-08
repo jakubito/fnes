7bafe7f1dfe8da1d2c95e827b5003f3c303c590f
Jakub Dobes <dobes.jakub@gmail.com>
2023-03-08 12:04:10 -0800

    Implement original pixel aspect ratio setting

--- src/client.ts
+++ src/client.ts
@@ -16,20 +16,29 @@ enum Keymap {
   ArrowRight = 0x64,
 }
 
+enum DisplayMode {
+  PixelPerfect = 1,
+  Original = 8 / 7,
+}
+
+const WIDTH = 256
+const HEIGHT = 240
+
 class Client {
   private module: CoreModule
-  instance: CoreInstance
+  readonly instance: CoreInstance
 
-  private frameBuffer: Uint8ClampedArray
-  private frameImageData: ImageData
-  private canvas: HTMLCanvasElement
-  private canvasContext: CanvasRenderingContext2D
+  private readonly timePerFrame = 1000 / 60
+  private readonly displayMode = DisplayMode.Original
+  private readonly frameBuffer: Uint8ClampedArray
+  private readonly frameImageData: ImageData
+  private readonly canvas: HTMLCanvasElement
+  private readonly canvasContext: CanvasRenderingContext2D
   private _stop?: () => void
 
   private _status = Status.Ready
   private _volume = 50
   private _screenScale = 3
-  private timePerFrame = 1000 / 60
 
   constructor(module: CoreModule) {
     this.module = module
@@ -37,8 +46,8 @@ class Client {
     // @ts-ignore
     const { buffer } = <WebAssembly.Memory>module.memory
     const frameBufferPointer = module.getFrameBufferPointer(this.instance)
-    this.frameBuffer = new Uint8ClampedArray(buffer, frameBufferPointer, 256 * 240 * 4)
-    this.frameImageData = new ImageData(this.frameBuffer, 256, 240)
+    this.frameBuffer = new Uint8ClampedArray(buffer, frameBufferPointer, WIDTH * HEIGHT * 4)
+    this.frameImageData = new ImageData(this.frameBuffer, WIDTH, HEIGHT)
     this.canvas = document.createElement('canvas')
     this.canvasContext = this.canvas.getContext('2d')!
     this.screenScale = this._screenScale
@@ -59,10 +68,10 @@ class Client {
 
   set screenScale(scale: number) {
     this._screenScale = scale
-    this.canvas.width = 256 * scale * window.devicePixelRatio
-    this.canvas.height = 240 * scale * window.devicePixelRatio
-    this.canvas.style.width = `${256 * scale}px`
-    this.canvas.style.height = `${240 * scale}px`
+    this.canvas.width = WIDTH * this.displayMode * scale * window.devicePixelRatio
+    this.canvas.height = HEIGHT * scale * window.devicePixelRatio
+    this.canvas.style.width = `${WIDTH * this.displayMode * scale}px`
+    this.canvas.style.height = `${HEIGHT * scale}px`
     this.canvasContext.imageSmoothingEnabled = false
   }
 
@@ -95,7 +104,17 @@ class Client {
       if (frameReady) {
         const { width, height } = this.canvas
         this.canvasContext.putImageData(this.frameImageData, 0, 0)
-        this.canvasContext.drawImage(this.canvas, 0, 0, 256, 240, 0, 0, width, height)
+        this.canvasContext.imageSmoothingEnabled = false
+
+        if (this.displayMode === DisplayMode.PixelPerfect) {
+          this.canvasContext.drawImage(this.canvas, 0, 0, WIDTH, HEIGHT, 0, 0, width, height)
+        } else {
+          const scaledWidth = WIDTH * this._screenScale * window.devicePixelRatio
+          this.canvasContext.drawImage(this.canvas, 0, 0, WIDTH, HEIGHT, 0, 0, scaledWidth, height)
+          this.canvasContext.imageSmoothingEnabled = true
+          this.canvasContext.drawImage(this.canvas, 0, 0, scaledWidth, height, 0, 0, width, height)
+        }
+
         frameReady = false
       }
 
